851ba30778a3777c4a59d6d5942332fc5d277e3f
noureddenh <nouredden@halogen.my>
2025-06-06 15:45:04 +0800

    commented cidr_block

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,7 +31,7 @@ resource "aws_route_table" "main" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    # cidr_block = "0.0.0.0/0"
     ipv6_cidr_block = "::/0"
     gateway_id = aws_internet_gateway.main.id
   }  
